22440d2937bb7d413ba62a50d330f1ce3d45250c
Mike Xiao <xiaokaic@cse.msu.edu>
2019-01-04 19:15:56 -0600

    more precise realtime age calculation

--- src/components/tables/SelfStatsTable.js
+++ src/components/tables/SelfStatsTable.js
@@ -2,26 +2,30 @@ import React, { Component } from 'react';
 import moment from "moment";
 import { Table, Tab, Flag } from 'semantic-ui-react';
 
-const birthdate = "11.09.1994 09:30:00";
+const birthdate = '1994-11-09T09:30:00';
 
 class SelfStatsTable extends Component {
   state = {}
   componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({ age: moment().diff(birthdate, "seconds") });
-      this.age = this.state.age / 3.154e+7;
-    }, 1000);
+    this.interval = setInterval(() => this.tick(), 25);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+
+  tick() {
+    const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
+    const birthTime = new Date(birthdate);
+    this.setState({ age: ((Date.now() - birthTime) / divisor).toFixed(10) });
+  }
+
   render() {
     return (
       <Table definition striped color='yellow'>
         <Table.Body>
           <Table.Row>
             <Table.Cell width={3}>Age</Table.Cell>
-            <Table.Cell width={5} textAlign='center'>{this.age}</Table.Cell>
+            <Table.Cell width={5} textAlign='center'>{this.state.age}</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Lives At</Table.Cell>
